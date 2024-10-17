4b17e7f7fd051ee23182155527fdbeec9bc7b32c
Killian Viquerat <kil.viquerat@gmail.com>
2024-10-17 13:23:35 +0200

    Add ImageToAscii little feature to have colored char using html format

--- src/main/java/ch/heigvd/dai/commands/Root.java
+++ src/main/java/ch/heigvd/dai/commands/Root.java
@@ -37,9 +37,20 @@ public class Root{
             description = "The size of the kernel used to average pixel together for the ascii effect (must be an odd number).",
             required = false
     )
+
     protected int size = 9;
 
+    @CommandLine.Option(
+            names = {"-c", "--color"},
+            description = "Precise the output for the ascii file(used = html file with colored char | unused = txt file with black and white char )",
+            required = false
+    )
+
+    protected boolean color = false;
+
+
     public int getSize() { return size; }
+    public boolean getColor() { return color; }
     public String getInFilename() {return filename;}
     public String getOutFilename()  {return output;}
 
